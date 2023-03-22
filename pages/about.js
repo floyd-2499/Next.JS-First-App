590f37020d2df136fe718959dae32d5299f80586
Floyd Manuel <floydmanuel011@gmail.com>
2023-03-23 00:22:07 +0530

    Initiating a test

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import {Header} from './index'
+
+export default function About() {
+    return (
+        <>
+        <Header />
+        </>
+    );
+}
